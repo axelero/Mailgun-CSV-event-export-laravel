8c68ee96d64c7d450509883c0c71b9f9130f2e20
tacone <tacone@users.noreply.github.com>
2017-01-05 17:45:40 +0100

    export all the fields

--- README.md
+++ README.md
@@ -4,4 +4,4 @@ A Mailgun API connector that exports events to a CSV file.
 # Steps
 - Rename app/database/production.sqlite.default to app/database/production.sqlite
 - Run migrations
-- Update your env file. Refer to .env.php file for instructions.
+- Create a .env.php file. Refer to .env.sample.php file for instructions.
--- app/src/ExportHelper.php
+++ app/src/ExportHelper.php
@@ -29,20 +29,23 @@ class ExportHelper
         $mailgun  = new Mailgun($apiKey);
         $response = $mailgun->get($uri);
         $events   = $response->http_response_body->items;
+        // recursive object to array
+        $events = json_decode(json_encode($events), true);
+
+        $fields = $this->getFields();
+
+        $row     = $fields;
+        fputcsv($file, $row);
 
         foreach ($events as $event) {
             $row     = [];
 
-            $createdOn = Carbon::createFromTimestampUTC((int)$event->timestamp);
+            $createdOn = Carbon::createFromTimestampUTC((int)$event['timestamp']);
             $row[]     = $createdOn->toDateTimeString();
 
-            $row[] = $event->event;
-
-            $headers = (array) $event->message->headers;
-            $parts   =  explode('@', $headers['message-id']);
-            $row[]   = $parts[0];
-
-            $row[] = $event->recipient;
+            foreach ($fields as $f) {
+                $row[] = array_get($event, $f);
+            }
 
             fputcsv($file, $row);
         }
@@ -52,5 +55,16 @@ class ExportHelper
         $parts = explode('/events/', $response->http_response_body->paging->next, 2);
 
         return array($response, count($events), "$domain/events/" . $parts[1]);
+    }
+
+    protected function getFields()
+    {
+        $files = glob(base_path('resources/samples').'/*.json');
+        $record = [];
+        foreach ($files as $f) {
+            $data = json_decode(file_get_contents($f), true);
+            $record = $record + array_dot($data);
+        }
+        return array_keys($record);
     }
 }
